3245b8cc31afe74780b5c4b6b555d9b856081616
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-06-15 15:43:19 +0800

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+[![license][license-image]][license-url]
+
+# 溫度換算
+[狀態提升練習]輸入攝氏(華氏)即可換算出華氏(攝氏)溫度
+
+
+## Demo
+連結
+
+<img src="./screenshot/img01.png">
+
+## LICENSE
+MIT
+
+
+[license-image]: https://img.shields.io/badge/license-MIT-blue.svg
+[license-url]: 
---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Calculator from './Calculator';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <Calculator />
+      </div>
+    );
+  }
+}
